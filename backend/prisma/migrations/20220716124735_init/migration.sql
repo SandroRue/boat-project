057f8490e3222f6628e3434fd5a3319e1fc3982f
SandroRue <sandro.ruettimann@outlook.com>
2022-07-16 14:57:26 +0200

    add database

---
+++ backend/prisma/migrations/20220716124735_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Boat" (
+    "id" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Boat_pkey" PRIMARY KEY ("id")
+);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,25 @@
 import cors from "cors";
 import express, { Request, Response } from "express";
+import { v4 } from "uuid";
+import prisma from './prisma-client'
 
 const main = async () => {
-
+    const uuid = () => v4()
     const app = express()
     app.use(express.json()).use(cors()).disable("x-powered-by")
+
+    const boat = await prisma.boat.create({
+        data: {
+            id: uuid(),
+            image: 'testImage',
+            name: 'speeder1',
+            description: '50 km/h speed'
+        },
+    })
+
+    const allBoats = await prisma.boat.findMany({})
+
+    console.log(allBoats)
 
     app.get("/", (req, res) => res.send("OK"))
 
