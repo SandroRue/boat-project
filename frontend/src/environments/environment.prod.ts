1902422ad0aea52671e6daaabe1477550852986d
SandroRue <sandro.ruettimann@outlook.com>
2022-07-24 09:46:40 +0200

    add render deployment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  angularBackendUrl: 'https://boat-backend.onrender.com',
+  firebase: {
+    apiKey: 'AIzaSyB2m_EbIyZolf74UdOsKy1Wmh4qGWsE8UU',
+    authDomain: 'boat-app-e2fbc.firebaseapp.com',
+    projectId: 'boat-app-e2fbc',
+    storageBucket: 'boat-app-e2fbc.appspot.com',
+    messagingSenderId: '1096246392871',
+    appId: '1:1096246392871:web:d31d1c6b3ee17697357602',
+    measurementId: 'G-NZ2YK9W557'
+  }
 };
